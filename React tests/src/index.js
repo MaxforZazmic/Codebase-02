2d2123dbbb2a1f4d2d665fef4298806a53ec06d7
Malkhaz Arabuli <malkhazarabuli@Malkhazs-MacBook-Air.local>
2022-10-21 23:13:10 +0400

    add code

---
+++ React tests/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Mentors </h1>
+
+    <ul>
+      <li>someone</li>
+      <li>someone</li>
+      <li>someone</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
