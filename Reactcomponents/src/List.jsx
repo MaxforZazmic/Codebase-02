64c93f3439efb35283f96c21631c89a4336f2303
Malkhaz Arabuli <malkhazarabuli@Malkhazs-MacBook-Air.local>
2022-10-26 14:36:04 +0400

    initial commit

---
+++ Reactcomponents/src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> Khachapuri </li>
+      <li> Khinkali </li>
+      <li> Mwvadi </li>
+    </ul>
+  );
+}
+
+export default List;
+
