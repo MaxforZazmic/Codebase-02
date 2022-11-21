629d7d991d4eb392bb2e1aa39696901028f89180
Malkhaz Arabuli <malkhazarabuli@Malkhazs-MacBook-Air.local>
2022-11-21 18:53:34 +0400

    push changes

--- refreshreact/src/App.js
+++ refreshreact/src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import "./App.css";
+import GoalList from "./Components/GoalList/GoalList";
+import NewGoal from "./Components/NewGoal/NewGoal";
 
 const APP = () => {
-  return <h1>React APP</h1>
+const [courseGoals, setCourseGoals] = useState([
+  {id: "cg1", text: "React refresh"},
+  {id: "cg2", text: "Node refresh"},
+  {id: "cg3", text: "Building MERN app"}
+]);
+
+const addNewGoalHandler = (newGoal) => {
+
+setCourseGoals ((prevCourseGoals) => {
+return prevCourseGoals.concat(newGoal);
+});
+};
+
+
+  return (        
+  <div className="course-goals">
+<h2>Course Goals</h2>
+<NewGoal onAddGoal={addNewGoalHandler}/>
+<GoalList goals={courseGoals} />
+  </div>
+  )
 };
 
 export default APP;
