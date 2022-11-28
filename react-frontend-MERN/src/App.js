12fa8c01e694cb2e3dc09dcb0efeccc586701e25
Malkhaz Arabuli <malkhazarabuli@Malkhazs-MacBook-Air.local>
2022-11-29 01:05:04 +0400

    update

--- react-frontend-MERN/src/App.js
+++ react-frontend-MERN/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from "./users/pages/Users";
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 
 const App = () => {
-  return <Router>
-
+  return (
+    <Router>
+<MainNavigation />
+<main>
 <Switch>
 <Route path="/" exact>  
 <Users /> 
@@ -18,8 +21,9 @@ const App = () => {
 
 <Redirect to="/" />
 </Switch>
-
-  </Router>;
+</main>
+  </Router>
+  );
 };
 
 
